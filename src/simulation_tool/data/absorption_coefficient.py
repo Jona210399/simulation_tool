c6554e7bca528185deb889850ea8929fca87dd74
Jona <jona.oestreicher@gmail.com>
2025-05-15 09:58:44 +0200

    refactor AbsorptionCoefficientData to support Path type and add Parquet file handling

--- src/simulation_tool/data/absorption_coefficient.py
+++ src/simulation_tool/data/absorption_coefficient.py
@@ -1,6 +1,8 @@
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
+from pathlib import Path
 
 import numpy as np
+import polars as pl
 from matplotlib.axes import Axes
 
 from simulation_tool.constants import M_TO_NM, NM_TO_M
@@ -9,15 +11,29 @@ from simulation_tool.typing_ import Array1D
 
 @dataclass
 class AbsorptionCoefficientData:
-    alpha: Array1D
     x: Array1D
+    alpha: Array1D
 
     @classmethod
-    def from_file(cls, file_path: str) -> "AbsorptionCoefficientData":
-        data = np.loadtxt(file_path)
+    def from_file(cls, file_path: Path) -> "AbsorptionCoefficientData":
+        if file_path.suffix == ".parquet":
+            data = pl.read_parquet(source=file_path)
+        else:
+            data = np.loadtxt(file_path)
+
         x = data[:, 0]
         absorption_coefficient = data[:, 1]
         return cls(absorption_coefficient, x)
+
+    def to_parquet(
+        self,
+        save_dir: Path,
+        dtype: pl.DataType = pl.Float32,
+    ):
+        save_location = save_dir / "alpha_of_x.parquet"
+        pl.DataFrame(asdict(self)).with_columns([pl.all().cast(dtype)]).write_parquet(
+            file=save_location
+        )
 
     def plot(
         self,
