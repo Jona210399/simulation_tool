42a0dafa8b27a4451f5db9468612b472960fe717
Jona <jona.oestreicher@gmail.com>
2025-05-15 17:12:53 +0200

    fix: add documentation for SET_UP_FILE and RUN_DIR_PREFIX constants

--- src/simulation_tool/constants.py
+++ src/simulation_tool/constants.py
@@ -13,7 +13,10 @@ NUM_TODO: int = 5
 
 
 SET_UP_FILE: str = "simulation_setup"
+"""Name of the setup file. This is used to store the device setup created from the SimssConfig."""
+
 RUN_DIR_PREFIX: str = "run"
+"""Prefix for the run directory. This is used to create the run directory for each simulation."""
 
 NUM_PHOTONS = 0.03 * 1e21
 """Number of photons that are added to the irradiance during EQE simulation. In this example it is 3% of the number of photons absorbed by a Silicon solar cell in m-2"""
